f22059dfb3a9b7d5bf34d61ba1bea412f04d5176
lpl-dev <leo.planquette@isen-ouest.yncrea.fr>
2020-03-25 16:11:11 +0100

    TP_11 EXERCICE_2

--- exercice_2.py
+++ exercice_2.py
@@ -8,24 +8,25 @@ class Complex:
 
     def __add__(self,other):
         if isinstance(other,Complex):
-            return Complex(self.__real+other.getReal(),self.__im+other.getIm())
+            return Complex(self.__real+other.__real,self.__im+other.__im)
         raise Exception('Opération invalide')
 
+    def __neg__(self):
+        return Complex(-self.__real,-self.__im)
+
     def __sub__(self, other):
-        if isinstance(other, Complex):
-            return Complex(self.__real-other.getReal(),self.__im-other.getIm())
-        raise Exception('Opération invalide')
+        return self+(-other)
 
     def __mul__(self, other):
         if isinstance(other, Complex):
-            return Complex(self.__real*other.getReal()-self.__im*other.getIm(),self.__real*other.getIm()+self.__im*other.getReal())
+            return Complex(self.__real*other.__real-self.__im*other.__im,self.__real*other.__im+self.__im*other.__real)
         raise Exception('Opération invalide')
 
     def __truediv__(self, other):
         if isinstance(other, Complex):
             r=self*other.conjug()
-            d=other.getReal()**2+other.getIm()**2
-            return Complex(r.getReal()/d,r.getIm()/d)
+            d=other.__real**2+other.__im**2
+            return Complex(r.__real/d,r.__im/d)
         raise Exception('Opération invalide')
 
     def __abs__(self):
@@ -33,7 +34,7 @@ class Complex:
 
     def __eq__(self, other):
         if isinstance(other, Complex):
-            return self.__real==other.getReal() and self.__im==other.getReal()
+            return self.__real==other.__real and self.__im==other.__im
         raise Exception('Condition invalide')
 
     def __ne__(self, other):
@@ -41,21 +42,15 @@ class Complex:
             return not self==other
         raise Exception('Condition invalide')
 
-    def getReal(self):
-        return self.__real
-
-    def getIm(self):
-        return self.__im
-
     def conjug(self):
         return Complex(self.__real,-self.__im)
 
 if __name__=='__main__':
     cplx1=Complex(1,1)
     cplx2=Complex(1,1)
-    print(cplx1+cplx2)
-    print(cplx1-cplx2)
-    print(cplx1*cplx2)
-    print(cplx1/cplx2)
-    print(cplx1==cplx2)
-    print(cplx1!=cplx2)
+    print(f'({cplx1}) + ({cplx2}) = {cplx1+cplx2}')
+    print(f'({cplx1}) - ({cplx2}) = {cplx1-cplx2}')
+    print(f'({cplx1}) * ({cplx2}) = {cplx1*cplx2}')
+    print(f'({cplx1}) / ({cplx2}) = {cplx1/cplx2}')
+    print(f'{cplx1} == {cplx2} : {cplx1==cplx2}')
+    print(f'{cplx1} != {cplx2} : {cplx1!=cplx2}')
