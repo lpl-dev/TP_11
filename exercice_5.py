6e427c4b2d338e5bcb53647ff906657e234d7ace
lpl-dev <leo.planquette@isen-ouest.yncrea.fr>
2020-03-30 19:29:40 +0200

    TP_11 EXERCICE_5

--- exercice_5.py
+++ exercice_5.py
@@ -5,7 +5,7 @@ class Matrix:
             if row_types.count(list)==len(data):
                 cols_per_row = [len(row) for row in data]
                 if len(data[0])==sum(cols_per_row)/len(data):
-                    self.__data=data
+                    self.data=data
                     self.shape=(len(data),len(data[0]))
                 else:
                     raise Exception("La matrice n'est pas valide : toutes les colonnes doivent avoir la même taille")
@@ -16,7 +16,7 @@ class Matrix:
 
     def __str__(self):
         render='[\n'
-        for row in self.__data:
+        for row in self.data:
             render+='\t['+' '.join(map(str,row))+']\n'
         render+=']'
         return render
@@ -27,7 +27,7 @@ class Matrix:
     def __add__(self, other):
         if isinstance(other,Matrix):
             if self.shape==other.shape:
-                return Matrix([[c1+c2 for c1,c2 in zip(r1,r2)] for r1,r2 in zip(self.__data,other.__data)])
+                return Matrix([[c1+c2 for c1,c2 in zip(r1,r2)] for r1,r2 in zip(self.data, other.data)])
             raise Exception('Opération invalide')
         raise Exception('Opération invalide')
 
@@ -35,7 +35,7 @@ class Matrix:
         return self+other
 
     def __neg__(self):
-        m=[[-col for col in row] for row in self.__data]
+        m=[[-col for col in row] for row in self.data]
         return Matrix(m)
 
     def __sub__(self,other):
@@ -53,7 +53,7 @@ class Matrix:
                     for k in range(other.shape[1]):
                         e=0
                         for j in range(self.shape[1]):
-                            e+=self.__data[i][j]*other.__data[j][k]
+                            e+= self.data[i][j] * other.data[j][k]
                         row.append(e)
                     m.append(row)
                 return Matrix(m)
@@ -66,27 +66,42 @@ class Matrix:
     def __lt__(self,other):
         if isinstance(other,Matrix):
             if self.shape==other.shape:
-                return Matrix([[c1<c2 for c1,c2 in zip(r1,r2)] for r1,r2 in zip(self.__data,other.__data)])
+                return Matrix([[c1<c2 for c1,c2 in zip(r1,r2)] for r1,r2 in zip(self.data, other.data)])
             raise Exception('Condition impossible')
         raise Exception('Condition impossible')
 
     def __gt__(self, other):
-        return Matrix([[not c for c in r] for r in (self<other).__data])
+        return Matrix([[not c for c in r] for r in (self<other).data])
 
     def transpose(self):
-        return Matrix([[self.__data[j][i] for j in range(self.shape[1])] for i in range(self.shape[0])])
+        return Matrix([[self.data[j][i] for j in range(self.shape[1])] for i in range(self.shape[0])])
 
     def trace(self):
         if self.shape[0]==self.shape[1]:
-            return sum([self.__data[i][i] for i in range(self.shape[0])])
+            return sum([self.data[i][i] for i in range(self.shape[0])])
         raise Exception("Le calcul de la trace n'est pas possible si la matrice n'est pas carrée")
+
+    def det(self):
+        if self.shape[0]==self.shape[1]:
+            return self.__det(self)
+        raise Exception("Le calcul du déterminant n'est pas possible si la matrice n'est pas carrée")
+
+    def __det(self,array):
+        if array.shape==(1,1):
+            return array.data[0][0]
+        else:
+            det=0
+            for i in range(array.shape[0]):
+                det+=(array.data[i][0] if i%2==0 else -array.data[i][0])*self.__det(Matrix([[c for cidx,c in enumerate(r) if cidx!=0] for ridx,r in enumerate(array.data) if ridx!=i]))
+            return det
+
 
 if __name__=='__main__':
     m1=Matrix([
         [1,2],
         [8,5]
     ])
-    m2 = Matrix([
+    m2=Matrix([
         [8, 2],
         [-5,7]
     ])
@@ -103,3 +118,10 @@ if __name__=='__main__':
     print(f'm1*=m2={m1}')
     print(f'm1<m2={m1<m2}')
     print(f'm1>m2={m1>m2}')
+    m3=Matrix([
+        [7,8,9,88],
+        [5,8,7,-98],
+        [-5,-6,3,55],
+        [7,8,2,3]
+    ])
+    print(f'Le déterminant de la matrice :\n{m3} est {m3.det()}')
